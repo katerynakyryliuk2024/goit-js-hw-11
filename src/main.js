cd254b23788b99fecadbd5eb09d27a1ab9cf75b7
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-01-18 14:10:40 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-console.log('');
+fetch('https://pixabay.com/api/')
+    .then(response => {
+  console.log(response);
+
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+      
+
+        return response.json();
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        if (err.message === '404') {
+              iziToast.error({
+    title: 'Error',
+    message: "Sorry, there are no images matching your search query. Please try again!",
+});
+        }
+        console.log(err);
+    });
+
